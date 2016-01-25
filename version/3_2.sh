0554fcfe5e41eacc16096a25a4a02f3a54c46f61
chandrakanth <chandrakanth@shippable.com>
2016-01-25 00:37:35 -0500

    Updates locaiton of pypy

--- version/3_2.sh
+++ version/3_2.sh
@@ -4,7 +4,7 @@
 sudo apt-get install -y python3.2 python3.2-dev
 
 # Install virtualenv
-sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O /tmp/ez_setup.py
+sudo wget https://github.com/himanshu0503/sample_python/blob/master/ez_setup.py -O /tmp/ez_setup.py
 sudo python3.2 /tmp/ez_setup.py
 sudo easy_install-3.2 pip
 pip3.2 install virtualenv
